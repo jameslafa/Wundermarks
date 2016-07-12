026255c959404c9c3d0ff578b6865f806f8616d0
James Lafa <jaaames.lf@gmail.com>
2016-07-12 13:47:01 +0200

    Tags are now links to search + add new color scheme

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @bookmarks = Bookmark.order(created_at: :desc)
+    if params[:q].present? and @q = params[:q]
+      @bookmarks = Bookmark.tagged_with(@q).order(created_at: :desc)
+    else
+      @bookmarks = Bookmark.order(created_at: :desc)
+    end
   end
 
   def tools
