4b1d3d863f86ef537568d9e93bf03dc7baec6acc
James Lafa <jaaames.lf@gmail.com>
2016-08-15 12:57:00 +0200

    Fix delicious import using XML instead of HTML parser

--- app/jobs/delicious_importer_job.rb
+++ app/jobs/delicious_importer_job.rb
@@ -2,12 +2,14 @@ class DeliciousImporterJob < ActiveJob::Base
   queue_as :low
 
   def perform(user, file_path)
+    Rails.logger.debug("Import for user [#{user.id}] file #{file_path}")
     # Open pseudo shitty html/xml file from delicious
     # Tags are not closed, it's a freaking mess
-    file = File.open(file_path) { |f| Nokogiri::XML(f) }
+    file = File.open(file_path) { |f| Nokogiri::HTML(f) }
 
     # We look for A tags. We have to drop the DD containing the description, it's impossible to parse them.
-    links = file.xpath("//A")
+    links = file.xpath("//a")
+    Rails.logger.debug("Found #{links.size} links")
 
     # We store the number of imported bookmarks
     imported_bookmarks_count = 0
@@ -19,20 +21,20 @@ class DeliciousImporterJob < ActiveJob::Base
     ActiveRecord::Base.transaction do
       # We look on every links
       links.each do |link|
-        url = link.attr("HREF")
+        url = link.attr("href")
 
         # Time is stored as a unix timestamp
-        created_at =  DateTime.strptime(link.attr("ADD_DATE"),'%s')
+        created_at =  DateTime.strptime(link.attr("add_date"),'%s')
 
         # Extract tags: guess what it's also a mess. But acts-as-taggable-on should clean it.
         # We also take only the first 5 ones, because we limit tags to 5 on Wundermarks
-        tag_list = ActsAsTaggableOn::TagList.new.add(link.attr("TAGS"), parse: true).take(5)
+        tag_list = ActsAsTaggableOn::TagList.new.add(link.attr("tags"), parse: true).take(5)
 
         # Extract the title, truncate it
         title =  link.content.truncate(Bookmark::MAX_TITLE_LENGTH)
 
         # On Delicious, it's either private or public. PRIVATE == "1" means private.
-        privacy = link.attr("PRIVATE").to_bool ? "only_me" : "everyone"
+        privacy = link.attr("private").to_bool ? "only_me" : "everyone"
 
         # Build a new bookmark
         bookmark = Bookmark.new(user: user, url: url, created_at: created_at, title: title, privacy: privacy, source: 'delicious')
