f593ca7f4038bb2fe9f98caee9f191cb0e8e3937
James Lafa <jaaames.lf@gmail.com>
2016-11-30 12:39:44 +0100

    Fx migrations

--- db/migrate/20161026055702_add_bookmarks_count_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddBookmarksCountToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :bookmarks_count, :integer
-    add_column :users, :wundermarks_count, :integer
-
-    add_index :users, :wundermarks_count
-  end
-end
--- db/migrate/20161130105703_remove_wundermarks_count_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveWundermarksCountFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :bookmarks_count
-    remove_column :users, :wundermarks_count
-  end
-end
